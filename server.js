3dc8413a7e309785d561d337b250bfb971e6c670
Aurino Geraldo <aurino_aj@hotmail.com>
2019-04-06 11:19:58 -0300

    adicionando morgan para logs simplificados

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const morgan = require('morgan')
 
 const port = process.env.PORT || 3000
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://localhost:27017/got', { useNewUrlParser: true });
 // Permitindo JSON
 app.use(express.json())
 app.use(cors());
+app.use(morgan(':method :url :status :response-time ms'))
 
 requireDir('./src/models')
 
--- src/controller/PersonagemController.js
+++ src/controller/PersonagemController.js
@@ -9,13 +9,13 @@ module.exports = {
     console.log(req.query)
     const result = await Personagem.paginate({}, {page, limit:10});
 
-    return res.json(result);
+    res.status(200).json(result);
   },
 
   async fetchOne(req, res) {
     const result = await Personagem.findById(req.params.id)
 
-    return res.json(result)
+    res.json(result)
   },
 
   async update(req, res){
@@ -23,18 +23,18 @@ module.exports = {
       new:true, // volta o objeto alterado. 
     });
 
-    return res.status(200).json(result)
+    res.status(200).json(result)
   },
 
   async delete(req, res){
     await Personagem.findByIdAndRemove(req.params.id)
 
-    return res.status(204).end()
+    res.status(204).end()
   },
 
   async create (req, res){
     const result = await Personagem.create(req.body);
 
-    return res.status(201).json(result)
+    res.status(201).json(result)
   }
 }
